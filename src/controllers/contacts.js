e917b1cb8d83825c90b6fac3c639a0f35937b8ef
Dima <dimapetrik416@gmail.com>
2025-08-10 16:51:29 -0500

    3.5

--- src/controllers/contacts.js
+++ src/controllers/contacts.js
@@ -1,18 +1,26 @@
+import * as contactsService from '../services/contacts.js'
 import createHttpError from 'http-errors'
-import * as contactsServices from '../services/contacts.js'
 
 export const getAllContacts = async (req, res) => {
-  const data = await contactsServices.getAllContacts()
-  res.json({ status: 200, message: 'Successfully found contacts!', data })
+  const contacts = await contactsService.getAllContacts()
+  res.json({
+    status: 200,
+    message: 'Contacts fetched successfully',
+    data: contacts,
+  })
 }
 
 export const getContactById = async (req, res) => {
   const { contactId } = req.params
-  const data = await contactsServices.getContactById(contactId)
-  if (!data) {
+  const contact = await contactsService.getContactById(contactId)
+  if (!contact) {
     throw createHttpError(404, 'Contact not found')
   }
-  res.json({ status: 200, message: 'Successfully found a contact!', data })
+  res.json({
+    status: 200,
+    message: 'Contact fetched successfully',
+    data: contact,
+  })
 }
 
 export const createContact = async (req, res) => {
@@ -20,32 +28,39 @@ export const createContact = async (req, res) => {
   if (!name || !phoneNumber || !contactType) {
     throw createHttpError(400, 'Missing required fields')
   }
-  const data = await contactsServices.createContact(req.body)
+  const newContact = await contactsService.createContact(req.body)
   res.status(201).json({
     status: 201,
     message: 'Successfully created a contact!',
-    data,
+    data: newContact,
   })
 }
 
 export const updateContact = async (req, res) => {
   const { contactId } = req.params
-  const updated = await contactsServices.updateContact(contactId, req.body)
-  if (!updated) {
+  const updatedContact = await contactsService.updateContact(
+    contactId,
+    req.body
+  )
+  if (!updatedContact) {
     throw createHttpError(404, 'Contact not found')
   }
   res.json({
     status: 200,
-    message: 'Successfully patched a contact!',
-    data: updated,
+    message: 'Contact updated successfully!',
+    data: updatedContact,
   })
 }
 
 export const deleteContact = async (req, res) => {
   const { contactId } = req.params
-  const deleted = await contactsServices.deleteContact(contactId)
-  if (!deleted) {
+  const deletedContact = await contactsService.deleteContact(contactId)
+  if (!deletedContact) {
     throw createHttpError(404, 'Contact not found')
   }
-  res.status(204).send()
+  res.json({
+    status: 200,
+    message: 'Contact deleted successfully',
+    data: deletedContact,
+  })
 }
--- src/server.js
+++ src/server.js
@@ -8,6 +8,7 @@ import { errorHandler } from './middlewares/errorHandler.js'
 
 function setupServer() {
   const app = express()
+  app.use(express.json())
 
   app.use(cors())
   app.use(pino())
@@ -15,10 +16,6 @@ function setupServer() {
 
   app.use(notFoundHandler)
   app.use(errorHandler)
-
-  app.use((req, res) => {
-    res.status(404).json({ message: 'Not found' })
-  })
 
   const PORT = process.env.PORT || 3000
   app.listen(PORT, () => {
